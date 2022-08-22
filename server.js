4828c054eae8178520d4423a2b5a73be3bbd28ab
Xueyao Yu <x224yu@v1040-wn-rt-a-3-27.campus-dynamic.uwaterloo.ca>
2022-08-22 18:06:47 -0400

    cleanup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(
     '/api',
     createProxyMiddleware({
-        target: 'https://gherald-backend.herokuapp.com',
+        target: process.env.NODE_ENV === "production" ? 'https://gherald-backend.herokuapp.com' : "http://localhost:8080",
         changeOrigin: true
     })
 );
