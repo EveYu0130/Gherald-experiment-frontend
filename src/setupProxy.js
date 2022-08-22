ec0c2db8ba8cf1fd6bd9f4a106df997db7de3e0e
Xueyao Yu <x224yu@Xueyaos-Air.lan>
2022-08-22 00:06:00 -0400

    proxy fix

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
-    app.use(
-        createProxyMiddleware('/api/**', {
-            target: 'https://gherald-backend.herokuapp.com',
-        })
-    );
-};
