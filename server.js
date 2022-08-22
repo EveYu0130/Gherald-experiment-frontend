e7eee78f7cd440e5b79f5dab5d7e6fabc09ee64a
Xueyao Yu <x224yu@v1040-wn-rt-a-3-27.campus-dynamic.uwaterloo.ca>
2022-08-22 16:58:18 -0400

    enable JS fix

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 const port = process.env.PORT || 3000;
 // const publicPath = path.join(__dirname, 'public');
@@ -7,6 +9,13 @@ console.log(port);
 // console.log(__dirname);
 // console.log(publicPath);
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(
+    '/api',
+    createProxyMiddleware({
+        target: 'https://gherald-backend.herokuapp.com',
+        changeOrigin: true
+    })
+);
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(publicPath, 'index.html'));
