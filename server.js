3ef3867c76a647a0fd75adc9e4ae7ce1344993d4
Xueyao Yu <x224yu@Xueyaos-Air.lan>
2022-08-24 03:10:11 -0400

    router fix

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(
     '/api',
     createProxyMiddleware({
-        target: process.env.NODE_ENV === "production" ? 'https://gherald-backend.herokuapp.com' : "http://localhost:8080",
+        target: 'https://gherald-backend.herokuapp.com',
         changeOrigin: true
     })
 );
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
