b38e6f332985b670d70eff0ac04c541ea93b15fe
Xueyao Yu <x224yu@Xueyaos-Air.lan>
2022-09-20 19:27:26 -0400

    add timer practice task

--- src/stepHandler.js
+++ src/stepHandler.js
@@ -1,9 +1,62 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import MainPage from "./Components/Pages/MainPage";
 import TaskA from "./Components/Pages/TaskA";
 import TaskB from "./Components/Pages/TaskB";
 import Questionnaire from "./Components/Pages/Questionnaire";
 import EndPage from "./Components/Pages/EndPage";
+import {
+    Button,
+    Dialog,
+    DialogActions, DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@mui/material";
+
+const PracticeTimer = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [alert, setAlert] = useState(false);
+
+    useEffect(() => {
+        if (seconds < 300) {
+            const interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1);
+            }, 1000);
+
+            return () => clearInterval(interval);
+        } else {
+            setAlert(true);
+        }
+    }, [seconds]);
+
+    const handleAlertClose = () => {
+        setAlert(false);
+    };
+
+    return (
+        <Dialog
+            open={alert}
+            onClose={handleAlertClose}
+            maxWidth="md"
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                You have spent 5 minutes on the practice tasks.
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description" sx={{py: 2}}>
+                    Please note that the practice round is just for getting you familiar with the task workflow, the UI, and the problem space.
+                    Please do not spend too much time conducting real code reviews and inspecting the code changes.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleAlertClose}>
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
 
 function StepHandler() {
     const steps = [
@@ -18,15 +71,18 @@ function StepHandler() {
     const [practiced, setPracticed] = useState(false);
     const CurrentStepComponent = steps[currentStep];
     return (
-        <CurrentStepComponent
-            onSubmit={()=>{
-                setCurrentStep((practice && currentStep == 2) ? 0 : currentStep + 1);
-            }}
-            practice={practice}
-            setPractice={setPractice}
-            practiced={practiced}
-            setPracticed={setPracticed}
-        />
+        <div>
+            {practice && !practiced && <PracticeTimer />}
+            <CurrentStepComponent
+                onSubmit={()=>{
+                    setCurrentStep((practice && currentStep == 2) ? 0 : currentStep + 1);
+                }}
+                practice={practice}
+                setPractice={setPractice}
+                practiced={practiced}
+                setPracticed={setPracticed}
+            />
+        </div>
     );
 }
 
