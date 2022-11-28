f9485e8ea7ba5977125b52d9b7271cc34e1f1e4b
Xueyao Yu <x224yu@v1040-wn-rt-a-61-248.campus-dynamic.uwaterloo.ca>
2022-11-28 15:04:39 -0500

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(
     '/api',
     createProxyMiddleware({
-        target: process.env.NODE_ENV === "production" ? 'https://gherald-backend.herokuapp.com' : "http://localhost:8080",
+        target: 'https://gherald-backend.herokuapp.com',
         changeOrigin: true
     })
 );
