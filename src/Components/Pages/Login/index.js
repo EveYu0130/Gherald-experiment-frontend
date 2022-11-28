9680951a45a5336c1bd162e7d17914049cbe846b
Xueyao Yu <x224yu@v1040-wn-rt-a-61-248.campus-dynamic.uwaterloo.ca>
2022-11-28 16:12:56 -0500

    test route path

--- src/Components/Pages/Login/index.js
+++ src/Components/Pages/Login/index.js
@@ -29,7 +29,7 @@ function Login() {
     const history = useHistory();
     let location = useLocation();
     let auth = useAuth();
-    let { from } = location.state || { from: { pathname: "/" } };
+    let { from } = location.state || { from: { pathname: "/gherald" } };
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
