74f0694a86a484a05e05b5fae7f020520efa7cb0
Xueyao Yu <x224yu@v1040-wn-rt-a-61-248.campus-dynamic.uwaterloo.ca>
2022-11-28 15:58:35 -0500

    update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 // const port = process.env.PORT || 3000;
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(
     '/api',
