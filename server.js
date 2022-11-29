20fd52ff08d9174b0a8288f636e64264024c0d4a
Xueyao Yu <x224yu@v1040-wn-rt-a-61-248.campus-dynamic.uwaterloo.ca>
2022-11-28 19:08:59 -0500

    fix cors

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const app = express();
 const port = process.env.PORT || 9000;
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.get('/*', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+})
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
