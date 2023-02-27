6a723c906144030d037bce3156306c5c7256c67a
Xueyao Yu <x224yu@Xueyaos-MacBook-Air.local>
2023-02-26 21:48:42 -0500

    bug fix

--- src/Components/Pages/Questionnaire/index.js
+++ src/Components/Pages/Questionnaire/index.js
@@ -120,7 +120,7 @@ function Questionnaire({onSubmit}) {
                                         <FormControlLabel value="Easy" control={<Radio />} label={t('questionnaire_q2_a1')} />
                                     </Box>
                                     <Box sx={{ width: 200, height: 30}}>
-                                        <FormControlLabel value="Moderate" control={<Radio />} label={t('questionnaire_q2_a1')} />
+                                        <FormControlLabel value="Moderate" control={<Radio />} label={t('questionnaire_q2_a2')} />
                                     </Box>
                                     <Box sx={{ width: 200, height: 30}}>
                                         <FormControlLabel value="Very hard" control={<Radio />} label={t('questionnaire_q2_a3')} />
