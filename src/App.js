fce64d5be6f0ca72ececd094acae79e7153cf6d7
Xueyao Yu <x224yu@v1040-wn-rt-a-123-84.campus-dynamic.uwaterloo.ca>
2023-03-21 17:35:00 -0400

    update datadog plan

--- src/App.js
+++ src/App.js
@@ -17,25 +17,23 @@ import { datadogLogs } from '@datadog/browser-logs';
 import { datadogRum } from '@datadog/browser-rum';
 
 datadogLogs.init({
-    clientToken: 'pub7dae0b6c3863c812027015af41ac22bd',
-    site: 'datadoghq.com',
+    clientToken: 'pubf4c48f798b09902ea5073413e425147a',
+    site: 'us5.datadoghq.com',
     forwardErrorsToLogs: true,
     sessionSampleRate: 100,
 })
 
 datadogRum.init({
-    applicationId: 'c403d18c-66d7-4a1b-8c42-38cc706496b8',
-    clientToken: 'pub7dae0b6c3863c812027015af41ac22bd',
-    site: 'datadoghq.com',
+    applicationId: '9e810cbc-fc19-4a3e-922d-b0651acc76fc',
+    clientToken: 'pubf4c48f798b09902ea5073413e425147a',
+    site: 'us5.datadoghq.com',
     service:'gherald',
 
     // Specify a version number to identify the deployed version of your application in Datadog
     // version: '1.0.0',
     sessionSampleRate: 100,
-    sessionReplaySampleRate: 100,
+    premiumSampleRate: 100,
     trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
     defaultPrivacyLevel:'mask-user-input'
 });
 
